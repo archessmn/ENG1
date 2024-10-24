a7975e8bcd86f0ac5c2cdf8beea02c2e7796b73c
Mia Moir <me@archess.mn>
2024-10-24 11:06:38 +0100

    Add a simple grid system and rendering

--- game/core/src/main/java/io/github/archessmn/ENG1/Main.java
+++ game/core/src/main/java/io/github/archessmn/ENG1/Main.java
@@ -5,20 +5,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import io.github.archessmn.ENG1.Util.GridUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+    public static final Integer VIEWPORT_WIDTH = 960;
+    public static final Integer VIEWPORT_HEIGHT = 540;
+
     private SpriteBatch batch;
     private Texture image;
     private Texture pipe;
+
+    ShapeRenderer gridRenderer;
+    ShapeRenderer shapeRenderer;
 
     Sound pipeDrop;
     Sprite logo;
@@ -26,6 +36,7 @@ public class Main extends ApplicationAdapter {
     FitViewport viewport;
 
     Vector2 touchPos;
+    Vector2 unprojectedTouchPos;
 
     Array<Sprite> dropSprites;
 
@@ -33,6 +44,13 @@ public class Main extends ApplicationAdapter {
 
     Rectangle logoRectangle;
     Rectangle dropRectangle;
+    BitmapFont font;
+    Boolean isClicked = false;
+    Boolean isLogoCLicked = false;
+
+    Boolean paused = false;
+
+    Integer score = 0;
 
     @Override
     public void create() {
@@ -43,16 +61,27 @@ public class Main extends ApplicationAdapter {
 
         logo = new Sprite(image);
         pipeSprite = new Sprite(pipe);
-        logo.setSize(1,1);
+        logo.setSize(60,60);
 
         touchPos = new Vector2();
+        unprojectedTouchPos = new Vector2();
 
         dropSprites = new Array<>();
 
-        viewport = new FitViewport(16, 9);
+        gridRenderer = new ShapeRenderer();
+        shapeRenderer = new ShapeRenderer();
+
+        viewport = new FitViewport(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
 
         logoRectangle = new Rectangle();
         dropRectangle = new Rectangle();
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("ui/Arial.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = (int)(0.05f * Gdx.graphics.getHeight());
+        font = generator.generateFont(parameter);
+        font.getData().setScale(viewport.getWorldHeight() / Gdx.graphics.getHeight());
+        generator.dispose();
 
         createDroplet();
     }
@@ -65,8 +94,8 @@ public class Main extends ApplicationAdapter {
     }
 
     private void createDroplet() {
-        float dropWidth = 1;
-        float dropHeight = 1;
+        float dropWidth = 60;
+        float dropHeight = 60;
         float worldWidth = viewport.getWorldWidth();
         float worldHeight = viewport.getWorldHeight();
 
@@ -83,32 +112,37 @@ public class Main extends ApplicationAdapter {
     }
 
     private void input() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) paused = !paused;
 
-        float speed = 4f;
-        float delta = Gdx.graphics.getDeltaTime();
+        if (paused) {
+            isLogoCLicked = false;
+            return;
+        };
 
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            logo.translateX((speed * delta));
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            logo.translateX(-(speed * delta));
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            logo.translateY((speed * delta));
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            logo.translateY(-(speed * delta));
+        touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+        unprojectedTouchPos.set(viewport.unproject(touchPos));
+
+        isClicked = Gdx.input.isTouched();
+
+        if (Gdx.input.justTouched()) {
+            isLogoCLicked = logoRectangle.contains(unprojectedTouchPos);
+        } else if (!isClicked) {
+            isLogoCLicked = false;
+
+            Vector2 gridCoords = GridUtils.getGridCoords(logo.getX() + logo.getWidth() / 2, logo.getY() + logo.getHeight() / 2);
+
+            logo.setCenter(gridCoords.x, gridCoords.y);
         }
 
-        if (Gdx.input.isTouched()) {
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
 
-            viewport.unproject(touchPos);
+        if (isLogoCLicked) {
             logo.setCenter(touchPos.x, touchPos.y);
         }
     }
 
     private void logic() {
+        if (paused) return;
+
         float worldWidth = viewport.getWorldWidth();
         float worldHeight = viewport.getWorldHeight();
 
@@ -128,7 +162,7 @@ public class Main extends ApplicationAdapter {
             float dropWidth = dropSprite.getWidth();
             float dropHeight = dropSprite.getHeight();
 
-            dropSprite.translateY(-2f * delta);
+            dropSprite.translateY(-120f * delta);
 
             dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
 
@@ -136,7 +170,7 @@ public class Main extends ApplicationAdapter {
             if (dropSprite.getY() < -dropHeight) dropSprites.removeIndex(i);
             else if (logoRectangle.overlaps(dropRectangle)) { // Check if the bucket overlaps the drop
                 dropSprites.removeIndex(i); // Remove the drop
-                logo.setSize(logoWidth * 1.1f, logoHeight * 1.1f);
+                score += 1;
                 pipeDrop.play();
             }
         }
@@ -157,6 +191,16 @@ public class Main extends ApplicationAdapter {
 
         batch.setProjectionMatrix(viewport.getCamera().combined);
 
+        //Draw grid using util function
+        GridUtils.drawGrid(gridRenderer);
+
+        // Draw red outline for where the logo will snap to
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(1f, 0, 0, 1f);
+        Vector2 logoCoords = GridUtils.getGridCoords(logo.getX() + logo.getWidth() / 2, logo.getY() + logo.getHeight() / 2);
+        shapeRenderer.rect(logoCoords.x - (logo.getWidth() / 2), logoCoords.y - (logo.getHeight() / 2), logoRectangle.width, logoRectangle.height);
+        shapeRenderer.end();
+
         batch.begin();
 
         logo.draw(batch);
@@ -164,6 +208,12 @@ public class Main extends ApplicationAdapter {
         for (Sprite dropSprite : dropSprites) {
             dropSprite.draw(batch);
         }
+
+        font.draw(batch, String.format("Score: %d", score), 0, 520);
+        font.draw(batch, String.format("touch x: %f touch y: %f", touchPos.x, touchPos.y), 0, 440);
+        font.draw(batch, String.format("touch x: %f touch y: %f", logoRectangle.x, logoRectangle.y), 0, 400);
+
+        if (paused) font.draw(batch, "Paused", 480, 400);
 
         batch.end();
     }
---
+++ game/core/src/main/java/io/github/archessmn/ENG1/Util/GridUtils.java
@@ -0,0 +1,48 @@
+package io.github.archessmn.ENG1.Util;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+import static io.github.archessmn.ENG1.Main.VIEWPORT_HEIGHT;
+import static io.github.archessmn.ENG1.Main.VIEWPORT_WIDTH;
+
+public class GridUtils {
+    public static void drawGrid(ShapeRenderer gridRenderer) {
+        gridRenderer.begin(ShapeRenderer.ShapeType.Line);
+
+        for (int v = 1; v < 9; v++) {
+            gridRenderer.line(0, 60 * v,960, 60 * v);
+        }
+
+        for (int h = 1; h < 16; h++) {
+            gridRenderer.line(60 * h, 0,60 * h, 540);
+        }
+
+        gridRenderer.end();
+
+    }
+
+
+    public static Vector2 getGridCoords(Vector2 pos) {
+        float gridX = calcGridXCoord(pos.x);
+        float gridY = calcGridYCoord(pos.y);
+        return new Vector2(gridX, gridY);
+    }
+
+    public static Vector2 getGridCoords(float x, float y) {
+        float gridX = calcGridXCoord(x);
+        float gridY = calcGridYCoord(y);
+        return new Vector2(gridX, gridY);
+    }
+
+    public static float calcGridXCoord(float coord) {
+        float gridWidth = (VIEWPORT_WIDTH / 16f);
+        return (MathUtils.round(coord / gridWidth + 0.5f) * gridWidth) - (gridWidth / 2f);
+    }
+
+    public static float calcGridYCoord(float coord) {
+        float gridHeight = (VIEWPORT_HEIGHT / 9f);
+        return (MathUtils.round(coord / (gridHeight) + 0.5f) * gridHeight) - (gridHeight / 2f);
+    }
+}
