655a1bb2d1f19b30ab03e02b74dcfd5adbf5ba5a
Mia Moir <me@archess.mn>
2024-11-08 22:20:00 +0000

    Update weekly pages

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -6,43 +6,76 @@ import { MantineProvider, Title } from "@mantine/core";
 import { AppLayout } from "./_components/AppShell";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { WeekList } from "./pages/weeks/page";
-import Week1 from "./pages/weeks/Week1";
 import LicensePage from "./_components/LicensePage";
+import Week1 from "./pages/weeks/Week1";
+import Week2 from "./pages/weeks/Week2";
+import Week3 from "./pages/weeks/Week3";
+import Week4 from "./pages/weeks/Week4";
+import Week5 from "./pages/weeks/Week5";
+import Week6 from "./pages/weeks/Week6";
+import Week7 from "./pages/weeks/Week7";
+import { ModalsProvider } from "@mantine/modals";
 
 export default function App() {
   return (
     <MantineProvider defaultColorScheme="dark">
-      <HashRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<AppLayout />}
-          >
+      <ModalsProvider>
+        <HashRouter>
+          <Routes>
             <Route
-              path=""
-              element={<Title>Hello World!</Title>}
-            />
-            <Route
-              path="test"
-              element={<Title>Test Page</Title>}
-            />
-            <Route
-              path="licenses"
-              element={<LicensePage />}
-            />
-            <Route path="weeks">
+              path="/"
+              element={<AppLayout />}
+            >
               <Route
-                index
-                element={<WeekList />}
+                path=""
+                element={<Title>Hello World!</Title>}
               />
               <Route
-                path="1"
-                element={<Week1 />}
+                path="test"
+                element={<Title>Test Page</Title>}
               />
+              <Route
+                path="licenses"
+                element={<LicensePage />}
+              />
+              <Route path="weeks">
+                <Route
+                  index
+                  element={<WeekList />}
+                />
+                <Route
+                  path="1"
+                  element={<Week1 />}
+                />
+                <Route
+                  path="2"
+                  element={<Week2 />}
+                />
+                <Route
+                  path="3"
+                  element={<Week3 />}
+                />
+                <Route
+                  path="4"
+                  element={<Week4 />}
+                />
+                <Route
+                  path="5"
+                  element={<Week5 />}
+                />
+                <Route
+                  path="6"
+                  element={<Week6 />}
+                />
+                <Route
+                  path="7"
+                  element={<Week7 />}
+                />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
-      </HashRouter>
+          </Routes>
+        </HashRouter>
+      </ModalsProvider>
     </MantineProvider>
   );
 }
--- website/src/pages/weeks/page.tsx
+++ website/src/pages/weeks/page.tsx
@@ -10,6 +10,36 @@ export function WeekList() {
         component={ReactNavLink}
         to="/weeks/1"
       />
+      <NavLink
+        label={"Week 2"}
+        component={ReactNavLink}
+        to="/weeks/2"
+      />
+      <NavLink
+        label={"Week 3"}
+        component={ReactNavLink}
+        to="/weeks/3"
+      />
+      <NavLink
+        label={"Week 4"}
+        component={ReactNavLink}
+        to="/weeks/4"
+      />
+      <NavLink
+        label={"Week 5"}
+        component={ReactNavLink}
+        to="/weeks/5"
+      />
+      <NavLink
+        label={"Week 6"}
+        component={ReactNavLink}
+        to="/weeks/6"
+      />
+      <NavLink
+        label={"Week 7"}
+        component={ReactNavLink}
+        to="/weeks/7"
+      />
     </>
   );
 }
